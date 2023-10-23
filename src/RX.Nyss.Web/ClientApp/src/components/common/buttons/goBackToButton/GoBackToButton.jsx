e4d1cee44f91fcbc305ccbef7e8e626bfd951bea
norakf <nora.futsaeter@gmail.com>
2023-10-23 12:55:43 +0200

    Go back button moved to upper left corner

--- src/RX.Nyss.Web/ClientApp/src/components/common/buttons/goBackToButton/GoBackToButton.jsx
+++ src/RX.Nyss.Web/ClientApp/src/components/common/buttons/goBackToButton/GoBackToButton.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Button } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
-export const GoBackToButton = ({ onClick, variant, children }) => (
+export const GoBackToButton = ({ onClick, variant, children, size }) => (
   <Button
     onClick={onClick}
     startIcon={<ArrowBackIcon />}
     variant={variant}
     color="primary"
+    size={size}
   >
     {children}
   </Button>
