f59a0641c7e7fb52cfc872f55b10188c60967cd4
Sondre Kanstad <sondrekan@gmail.com>
2023-11-09 11:17:25 +0100

    Update getSaveFormModel function to handle nullable parameters

--- src/RX.Nyss.Web/ClientApp/src/components/projects/logic/projectsService.js
+++ src/RX.Nyss.Web/ClientApp/src/components/projects/logic/projectsService.js
@@ -1,17 +1,17 @@
-export const getSaveFormModel = (values, healthRisks) =>
-  ({
-    name: values.name,
-    allowMultipleOrganizations: values.allowMultipleOrganizations,
-    timeZoneId: values.timeZoneId,
-    healthRisks: healthRisks.map(healthRisk => ({
-      id: values[`healthRisk.${healthRisk.healthRiskId}.projectHealthRiskId`],
+export const getSaveFormModel = (values, healthRisks) => (
+    {
+    name: values?.name,
+    allowMultipleOrganizations: values?.allowMultipleOrganizations,
+    timeZoneId: values?.timeZoneId,
+    healthRisks: healthRisks && healthRisks.map(healthRisk => ({
+      id: healthRisk.id,
       healthRiskId: healthRisk.healthRiskId,
-      feedbackMessage: values[`healthRisk.${healthRisk.healthRiskId}.feedbackMessage`],
-      caseDefinition: values[`healthRisk.${healthRisk.healthRiskId}.caseDefinition`],
-      alertRuleCountThreshold: parseInt(values[`healthRisk.${healthRisk.healthRiskId}.alertRuleCountThreshold`]),
-      alertRuleDaysThreshold: parseInt(values[`healthRisk.${healthRisk.healthRiskId}.alertRuleDaysThreshold`]),
-      alertRuleKilometersThreshold: parseInt(values[`healthRisk.${healthRisk.healthRiskId}.alertRuleKilometersThreshold`])
+      feedbackMessage: healthRisk.feedbackMessage,
+      caseDefinition: healthRisk.caseDefinition,
+      alertRuleCountThreshold: parseInt(healthRisk.alertRuleCountThreshold),
+      alertRuleDaysThreshold: parseInt(healthRisk.alertRuleDaysThreshold),
+      alertRuleKilometersThreshold: parseInt(healthRisk.alertRuleKilometersThreshold)
     })),
-    organizationId: values.organizationId ? parseInt(values.organizationId) : null,
-    alertNotHandledNotificationRecipientId: parseInt(values.alertNotHandledNotificationRecipientId)
+    organizationId: values && values.organizationId ? parseInt(values?.organizationId) : null,
+    alertNotHandledNotificationRecipientId: parseInt(values?.alertNotHandledNotificationRecipientId)
   });
