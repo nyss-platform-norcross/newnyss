5d3693761be27762c44e7de4768e881620c838ae
Ian Evangelista <ian-angelo.roman.evangelista@redcross.no>
2023-11-15 16:18:40 +0100

    Fix collapse button rtl

--- src/RX.Nyss.Web/ClientApp/src/components/common/buttons/expandButton/ExpandButton.jsx
+++ src/RX.Nyss.Web/ClientApp/src/components/common/buttons/expandButton/ExpandButton.jsx
@@ -1,38 +1,41 @@
 import React from "react";
 import { IconButton, SvgIcon, makeStyles, useTheme, useMediaQuery } from '@material-ui/core';
 import { ReactComponent as ExpandLeftSVG } from "../../../../assets/icons/Expand-left.svg"
-
-const useStyles = makeStyles({
-  triangleBackground: {
-    position: "absolute",
-    zIndex: 1201,
-    right: -10,
-    top: 5,
-    height: 35,
-    width: 35,
-    transform: 'rotate(45deg)',
-    backgroundImage: 'linear-gradient(45deg, rgba(255,0,0,0) 50%, #F4F4F4 50%)',
-    borderRadius: '10px',
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  button: {
-    padding: 0,
-    transform: 'rotate(135deg) translateX(-5px)',
-    color: "#D52B1E",
-    '&:hover': {
-      backgroundColor: 'transparent',
-    },
-  },
-  invertedButton: {
-    transform: 'rotate(-45deg)',
-  },
-})
+import { useSelector } from "react-redux";
 
 export const ExpandButton = ({ onClick, isExpanded }) => {
-  const classes = useStyles();
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const useRtlDirection = useSelector(state => state.appData.direction === 'rtl');
+
+  const useStyles = makeStyles({
+    triangleBackground: {
+      position: "absolute",
+      zIndex: 1201,
+      right: useRtlDirection ? undefined : -10,
+      left: useRtlDirection ? -10 : undefined,
+      top: 5,
+      height: 35,
+      width: 35,
+      transform: useRtlDirection ? 'rotate(225deg)' : 'rotate(45deg)',
+      backgroundImage: useRtlDirection ? 'linear-gradient(-315deg, rgba(255,0,0,0) 50%, #F4F4F4 50%)' : 'linear-gradient(45deg, rgba(255,0,0,0) 50%, #F4F4F4 50%)',
+      borderRadius: '10px',
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    button: {
+      padding: 0,
+      transform: useRtlDirection ? 'rotate(-225deg) translateX(-5px)' : 'rotate(135deg) translateX(-5px)',
+      color: "#D52B1E",
+      '&:hover': {
+        backgroundColor: 'transparent',
+      },
+    },
+    invertedButton: {
+      transform: 'rotate(-45deg)',
+    },
+  })
+  const classes = useStyles();
 
   // The expanded button should only appear for larger screens
   if (isSmallScreen) return null;
@@ -42,7 +45,7 @@ export const ExpandButton = ({ onClick, isExpanded }) => {
       className={`${classes.button} ${isExpanded && classes.invertedButton}`}
       onClick={onClick}
     >
-       <SvgIcon style={{fontSize: 18}} component={ExpandLeftSVG} viewBox="0 0 48 49"/>
+      <SvgIcon style={{fontSize: 18}} component={ExpandLeftSVG} viewBox="0 0 48 49"/>
     </IconButton>
     </div>
   );
