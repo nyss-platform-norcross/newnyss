5f14fe42d31a22c552c54ce3817bb534b7c2de9f
Sondre Kanstad <sondrekan@gmail.com>
2023-11-24 10:15:55 +0100

    Sort steps by stepnumber to correct mapping of step icon and label

--- src/RX.Nyss.Web/ClientApp/src/components/common/stepper/SetupStepper.jsx
+++ src/RX.Nyss.Web/ClientApp/src/components/common/stepper/SetupStepper.jsx
@@ -83,6 +83,7 @@ const getStepContent = (steps, stepIndex) => {
 export const SetupStepper = ({ steps, error, setError, isNextStepInvalid, setIsNextStepInvalid }) => {
   const classes = useStyles();
   const [activeStep, setActiveStep] = useState(0);
+  const sortedSteps = steps.sort((stepA, stepB) => stepA.stepNumber - stepB.stepNumber);
 
   const handleNext = () => {
     if(!error && !isNextStepInvalid) {
@@ -124,7 +125,7 @@ export const SetupStepper = ({ steps, error, setError, isNextStepInvalid, setIsN
   return (
     <>
       <Stepper activeStep={activeStep} alternativeLabel className={classes.stepper} connector={<Connector />}>
-        {steps.map((step) => (
+        {sortedSteps.map((step) => (
           <Step className={classes.step} key={step.name} onClick={() => onStepClick(step.stepNumber)}>
             <StepLabel StepIconComponent={StepIcon}>
               <Typography style={{ color: step.stepNumber >= activeStep ? "#7C7C7C" : "#D52B1E"}}>{step.name}</Typography>
