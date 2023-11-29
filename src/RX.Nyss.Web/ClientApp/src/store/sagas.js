4c743c4d10af94031bcbefb7da6e86e65a4c7a29
Sondre Kanstad <sondrekan@gmail.com>
2023-11-29 10:10:14 +0100

    Comment in project setup

--- src/RX.Nyss.Web/ClientApp/src/store/sagas.js
+++ src/RX.Nyss.Web/ClientApp/src/store/sagas.js
@@ -5,7 +5,7 @@ import { authSagas } from "../authentication/authSagas";
 import { nationalSocietiesSagas } from "../components/nationalSocieties/logic/nationalSocietiesSagas";
 import { smsGatewaysSagas } from "../components/smsGateways/logic/smsGatewaysSagas";
 import { projectsSagas } from "../components/projects/logic/projectsSagas";
-//import { projectSetupSagas } from "../components/projectSetup/logic/projectSetupSagas";
+import { projectSetupSagas } from "../components/projectSetup/logic/projectSetupSagas";
 import { globalCoordinatorsSagas } from "../components/globalCoordinators/logic/globalCoordinatorsSagas";
 import { healthRisksSagas } from "../components/healthRisks/logic/healthRisksSagas";
 import { suspectedDiseaseSagas } from "../components/suspectedDiseases/logic/suspectedDiseaseSagas";
@@ -37,7 +37,7 @@ function* rootSaga() {
     ...eidsrIntegrationSagas(),
     ...organizationsSagas(),
     ...projectsSagas(),
-    //...projectSetupSagas(),
+    ...projectSetupSagas(),
     ...projectDashboardSagas(),
     ...projectOrganizationsSagas(),
     ...projectAlertRecipientsSagas(),
