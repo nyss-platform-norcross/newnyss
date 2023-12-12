51d6a9bc90bd6ff3ce91d5c77a9bfd00af35ab27
Alvar Hønsi <alvarhonsi@gmail.com>
2023-12-12 15:20:46 +0100

    Add track event helper function

--- src/RX.Nyss.Web/ClientApp/src/utils/appInsightsHelper.js
+++ src/RX.Nyss.Web/ClientApp/src/utils/appInsightsHelper.js
@@ -1,6 +1,12 @@
 import { getAppInsights } from "../components/app/ApplicationInsightsProvider";
+import { useTrackEvent } from "@microsoft/applicationinsights-react-js";
 
 export const trackPageView = (name) => {
   const appInsights = getAppInsights();
   appInsights.trackPageView({ name });
+};
+
+export const trackEvent = (name, properties) => {
+  const appInsights = getAppInsights();
+  appInsights.trackEvent({ name, properties });
 };
