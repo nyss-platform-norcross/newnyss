0967715f9ea7ab037dbbe9c0bd1ce93a89cb68c6
Ian Evangelista <ian-angelo.roman.evangelista@redcross.no>
2024-01-18 11:13:52 +0100

    Add drawer filter for alerts

--- src/RX.Nyss.Web/ClientApp/src/components/alerts/components/AlertsFilters.js
+++ src/RX.Nyss.Web/ClientApp/src/components/alerts/components/AlertsFilters.js
@@ -5,6 +5,8 @@ import {
   MenuItem,
   Card,
   CardContent,
+  useMediaQuery,
+  useTheme,
 } from "@material-ui/core";
 import LocationFilter from "../../common/filters/LocationFilter";
 import { strings, stringKeys } from "../../../strings";
@@ -13,6 +15,7 @@ import { DatePicker } from "../../forms/DatePicker";
 import { convertToLocalDate, convertToUtc } from "../../../utils/date";
 import useLocalFilters from "../../common/filters/useLocalFilters";
 import useLocationFilter from "../../common/filters/useLocationFilter";
+import { DrawerFilter } from "../../common/filters/DrawerFilter";
 
 export const AlertsFilters = ({
   filters,
@@ -21,6 +24,8 @@ export const AlertsFilters = ({
   onChange,
   rtl,
 }) => {
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
   //Reducer for local filters state
   const [localFilters, updateLocalFilters] = useLocalFilters(filters);
 
@@ -61,78 +66,92 @@ export const AlertsFilters = ({
     return null;
   }
 
+  const Filter = () => {
+    return (
+      <Grid container spacing={2} direction={isSmallScreen ? "column" : "row"} alignItems={isSmallScreen && "center"} >
+        <Grid item>
+          <DatePicker
+            select
+            label={strings(stringKeys.dashboard.filters.startDate)}
+            value={convertToLocalDate(localFilters.startDate)}
+            onChange={handleDateFromChange}
+            className={styles.filterItem}
+            InputLabelProps={{ shrink: true }}
+          ></DatePicker>
+        </Grid>
+        <Grid item>
+          <DatePicker
+            select
+            label={strings(stringKeys.dashboard.filters.endDate)}
+            value={convertToLocalDate(localFilters.endDate)}
+            onChange={handleDateToChange}
+            className={styles.filterItem}
+            InputLabelProps={{ shrink: true }}
+          ></DatePicker>
+        </Grid>
+        <Grid item>
+          <LocationFilter
+            filteredLocations={localFilters.locations}
+            allLocations={locations}
+            filterLabel={locationsFilterLabel}
+            onChange={handleLocationChange}
+            rtl={rtl}
+          />
+        </Grid>
+
+        <Grid item>
+          <TextField
+            select
+            label={strings(stringKeys.alerts.filters.healthRisks)}
+            onChange={handleHealthRiskChange}
+            value={localFilters.healthRiskId || 0}
+            className={styles.filterItem}
+            InputLabelProps={{ shrink: true }}
+          >
+            <MenuItem value={0}>
+              {strings(stringKeys.alerts.filters.healthRisksAll)}
+            </MenuItem>
+
+            {healthRisks.map((hr) => (
+              <MenuItem key={`filter_healthRisk_${hr.id}`} value={hr.id}>
+                {hr.name}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+
+        <Grid item>
+          <TextField
+            select
+            label={strings(stringKeys.alerts.filters.status)}
+            value={localFilters.status || "All"}
+            onChange={handleStatusChange}
+            className={styles.filterItem}
+            InputLabelProps={{ shrink: true }}
+          >
+            {Object.values(alertStatusFilters).map((status) => (
+              <MenuItem key={`filter_status_${status}`} value={status}>
+                {strings(stringKeys.alerts.constants.alertStatus[status])}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+      </Grid>
+    )
+  }
+
+  if(isSmallScreen) {
+    return (
+      <Grid container justifyContent="center" style={{ marginBottom: 20 }}>
+        <DrawerFilter title={strings(stringKeys.alerts.title)} children={<Filter/>} showResults={handleFiltersChange}/>
+      </Grid>
+    );
+  }
+
   return (
     <Card className={styles.filters}>
       <CardContent>
-        <Grid container spacing={2}>
-          <Grid item>
-            <DatePicker
-              select
-              label={strings(stringKeys.dashboard.filters.startDate)}
-              value={convertToLocalDate(localFilters.startDate)}
-              onChange={handleDateFromChange}
-              className={styles.filterItem}
-              InputLabelProps={{ shrink: true }}
-            ></DatePicker>
-          </Grid>
-          <Grid item>
-            <DatePicker
-              select
-              label={strings(stringKeys.dashboard.filters.endDate)}
-              value={convertToLocalDate(localFilters.endDate)}
-              onChange={handleDateToChange}
-              className={styles.filterItem}
-              InputLabelProps={{ shrink: true }}
-            ></DatePicker>
-          </Grid>
-          <Grid item>
-            <LocationFilter
-              filteredLocations={localFilters.locations}
-              allLocations={locations}
-              filterLabel={locationsFilterLabel}
-              onChange={handleLocationChange}
-              rtl={rtl}
-            />
-          </Grid>
-
-          <Grid item>
-            <TextField
-              select
-              label={strings(stringKeys.alerts.filters.healthRisks)}
-              onChange={handleHealthRiskChange}
-              value={localFilters.healthRiskId || 0}
-              className={styles.filterItem}
-              InputLabelProps={{ shrink: true }}
-            >
-              <MenuItem value={0}>
-                {strings(stringKeys.alerts.filters.healthRisksAll)}
-              </MenuItem>
-
-              {healthRisks.map((hr) => (
-                <MenuItem key={`filter_healthRisk_${hr.id}`} value={hr.id}>
-                  {hr.name}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Grid>
-
-          <Grid item>
-            <TextField
-              select
-              label={strings(stringKeys.alerts.filters.status)}
-              value={localFilters.status || "All"}
-              onChange={handleStatusChange}
-              className={styles.filterItem}
-              InputLabelProps={{ shrink: true }}
-            >
-              {Object.values(alertStatusFilters).map((status) => (
-                <MenuItem key={`filter_status_${status}`} value={status}>
-                  {strings(stringKeys.alerts.constants.alertStatus[status])}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Grid>
-        </Grid>
+        <Filter/>
       </CardContent>
     </Card>
   );
