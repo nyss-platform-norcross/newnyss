26fc32270b20d65ab591dfcbc67a7ac207d6c3b8
Alvar Hønsi <alvarhonsi@gmail.com>
2024-03-01 11:00:49 +0100

    Add group by epiWeek to KeptReportsInEscalatedAlerts histogram

--- src/RX.Nyss.Web/ClientApp/src/components/dashboardCharts/DashboardKeptReportByHealthRiskChart.jsx
+++ src/RX.Nyss.Web/ClientApp/src/components/dashboardCharts/DashboardKeptReportByHealthRiskChart.jsx
@@ -5,29 +5,81 @@ import {
   CardHeader,
   Typography,
   Chip,
+  Box,
+  useTheme,
 } from "@material-ui/core";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import { strings, stringKeys } from "../../strings";
 import useHoverChartTracking from "../../utils/useHoverChartTracking";
-import { eachDayOfInterval, parseISO, format } from "date-fns";
+import {
+  getDateStringInterval,
+  getEpiWeekStringInterval,
+} from "../../utils/dateStrings";
 
-const getOptions = (valuesLabel, series, categories) => ({
+const getOptions = (
+  valuesLabel,
+  series,
+  categories,
+  groupingType,
+  theme,
+  yMax = null,
+) => ({
   chart: {
     type: "column",
   },
   title: {
-    text: "",
+    text: valuesLabel,
   },
   xAxis: {
     categories: categories,
+    labels: {
+      formatter: function () {
+        switch (groupingType) {
+          case "Day":
+            const date = this.value.split("-");
+            return `${this.value}`;
+          case "Week":
+            const yearandWeek = this.value.split("/");
+            return categories.length > 25
+              ? `W${yearandWeek[1]} Y${yearandWeek[0].slice(-2)}`
+              : `W${yearandWeek[1]}<br/>Y${yearandWeek[0].slice(-2)}`;
+        }
+      },
+    },
+    tickmarkPlacement: "on",
+    tickInterval: 1,
+    tickWidth: 1,
+    lineColor: theme.palette.text.secondary,
+    lineWidth: 2,
+    tickColor: theme.palette.text.secondary,
   },
   yAxis: {
     title: {
       text: valuesLabel,
     },
     allowDecimals: false,
+    min: 0,
+    max: yMax,
+    tickInterval: 1,
+    gridLineColor: "white",
+    lineWidth: 2,
+    gridZIndex: 6,
+    stackLabels: {
+      enabled: true,
+    },
   },
+  colors: [
+    "#058DC7",
+    "#50B432",
+    "#ED561B",
+    "#DDDF00",
+    "#24CBE5",
+    "#64E572",
+    "#FF9655",
+    "#FFF263",
+    "#6AF9C4",
+  ],
   legend: {
     enabled: true,
     itemStyle: { fontWeight: "regular" },
@@ -38,10 +90,11 @@ const getOptions = (valuesLabel, series, categories) => ({
   plotOptions: {
     column: {
       pointPadding: 0,
-      borderWidth: 0,
+      borderWidth: 1,
       groupPadding: 0,
       shadow: false,
       stacking: "normal",
+      animation: false,
     },
   },
   tooltip: {
@@ -54,13 +107,24 @@ const getOptions = (valuesLabel, series, categories) => ({
 // Function to parse input data and generate categories and series data for charting.
 // The function takes a data object containing startDateString, endDateString, and groupedReports.
 const getCategoryAndSeriesData = (data) => {
-  const startDate = parseISO(data.chartStartDateString);
-  const endDate = parseISO(data.chartEndDateString);
-
   // Generate categories as an array of date strings for each day in the interval between startDate and endDate.
-  const categories = eachDayOfInterval({ start: startDate, end: endDate }).map(
-    (day) => format(day, "yyyy-MM-dd"),
-  );
+  let categories;
+  switch (data.dateGroupingType) {
+    case "Week":
+      categories = getEpiWeekStringInterval(
+        data.chartStartDateEpiWeekString,
+        data.chartEndDateEpiWeekString,
+      );
+      break;
+    case "Day":
+      categories = getDateStringInterval(
+        data.chartStartDateString,
+        data.chartEndDateString,
+      );
+      break;
+    default:
+      throw new Error(`Invalid date grouping type: ${dateGroupingType}`);
+  }
 
   // Helper function to aggregate series data based on the categories and input data.
   const getAggregatedSeriesData = (categories, data) => {
@@ -100,14 +164,30 @@ export const DashboardKeptReportByHealthRiskChart = ({ data }) => {
     }
   };
 
+  // Find the highest value for y by summing series
+  let yMax;
+  if (series.length > 0) {
+    let maxColumnValues = series.reduce((prev, curr) => {
+      const columnValues = prev.data.map((val, idx) => val + curr.data[idx]);
+      return { data: columnValues };
+    }).data;
+    yMax = Math.max(Math.ceil(Math.max(...maxColumnValues) / 5) * 5, 10); // Round Max y up to the closest multiple of 5 (10 minimum)
+  }
+
   series =
     selectedHealthRisk != null
       ? series.filter((s) => s.healthRiskId === selectedHealthRisk)
       : series;
+
+  const theme = useTheme();
+
   const chartData = getOptions(
-    strings(stringKeys.dashboard.reportsPerHealthRisk.numberOfReports, true),
+    "",
     series,
     categories,
+    data.dateGroupingType,
+    theme,
+    yMax,
   );
 
   return (
@@ -119,14 +199,21 @@ export const DashboardKeptReportByHealthRiskChart = ({ data }) => {
       <CardHeader
         title={
           <Typography variant="h5">
-            {strings(stringKeys.dashboard.reportsPerHealthRisk.title)}
+            {/* Add to string keys */}
+            {"Kept reports in escalated alerts"}
+          </Typography>
+        }
+        subheader={
+          <Typography variant="subtitle2">
+            {/* Add to string keys */}
+            {`Data from: ${data.chartStartDateString} to ${data.chartEndDateString}`}
           </Typography>
         }
       />
       <CardContent>
-        <div>
+        <Box mb={"1rem"}>
           <Chip
-            label={"All"}
+            label={"All"} // TODO: Make string key
             style={{
               marginRight: "10px",
               fontWeight: selectedHealthRisk ? "normal" : "bold",
@@ -147,7 +234,7 @@ export const DashboardKeptReportByHealthRiskChart = ({ data }) => {
               onClick={() => toggleHealthRisk(healthRiskReports.healthRiskId)}
             />
           ))}
-        </div>
+        </Box>
         <HighchartsReact
           highcharts={Highcharts}
           ref={resizeChart}
