f291c3f25737dc04850cbb55bb8887c8a533191c
Ian Evangelista <ian-angelo.roman.evangelista@redcross.no>
2024-03-07 12:59:34 +0100

    Changel layout of actions when escalating alert

--- src/RX.Nyss.Web/ClientApp/src/components/alerts/components/AlertsAssessmentActions.js
+++ src/RX.Nyss.Web/ClientApp/src/components/alerts/components/AlertsAssessmentActions.js
@@ -1,9 +1,6 @@
-import styles from "./AlertsAssessmentActions.module.scss";
-
 import React, { Fragment, useState, useEffect } from "react";
 import { stringKeys, strings } from "../../../strings";
 import SubmitButton from "../../common/buttons/submitButton/SubmitButton";
-import FormActions from "../../forms/formActions/FormActions";
 import CancelButton from "../../common/buttons/cancelButton/CancelButton";
 import { AlertsEscalationDialog } from "./AlertsEscalationDialog";
 import { assessmentStatus } from "../logic/alertsConstants";
@@ -11,7 +8,7 @@ import { AlertsCloseDialog } from "./AlertsCloseDialog";
 import { AlertsEscalationWithoutNotificationDialog } from "./AlertsEscalationWithoutNotificationDialog";
 import CheckboxField from "../../forms/CheckboxField";
 import { validators, createForm } from "../../../utils/forms";
-import { Button, CircularProgress, Grid, Snackbar } from "@material-ui/core";
+import { Button, CircularProgress, Grid, Snackbar, useMediaQuery} from "@material-ui/core";
 import { Alert, AlertTitle } from "@material-ui/lab";
 import { trackEvent } from "../../../utils/appInsightsHelper";
 
@@ -33,6 +30,8 @@ export const AlertsAssessmentActions = ({
 
   const [openValidationSnackbar, setOpenValidationSnackbar] =
     React.useState(false);
+
+  const isSmallScreen = useMediaQuery(theme => theme.breakpoints.down("sm"));
 
   useEffect(() => {
     if (validateEidsrResult) {
@@ -74,158 +73,156 @@ export const AlertsAssessmentActions = ({
   };
 
   return (
-    <Fragment>
-      <FormActions>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            {isNationalSocietyEidsrEnabled && (
-              <>
-                <div hidden="true">
-                  <Button
-                    disabled={props.isLoadingValidateEidsr}
-                    color="primary"
-                    onClick={() => props.validateEidsr(alertId)}
-                  >
-                    {" "}
-                    Validate Eidsr migration{" "}
-                  </Button>
-                </div>
-                {props.isLoadingValidateEidsr && <CircularProgress size={12} />}
-                <Snackbar open={openValidationSnackbar}>
-                  <Alert
-                    style={{ backgroundColor: "#eee" }}
-                    onClose={handleCloseValidationSnackbar}
-                    severity="info"
-                  >
-                    <AlertTitle>
-                      {strings(
-                        stringKeys.alerts.assess.alert.eidsrValidation.title,
-                      )}
-                    </AlertTitle>
-                    <ul>
-                      <li>
-                        {strings(
-                          stringKeys.alerts.assess.alert.eidsrValidation
-                            .connection,
-                        )}
-                        :{" "}
-                        {validateEidsrResult?.isEidsrApiConnectionRunning
-                          ? strings(stringKeys.common.boolean.true)
-                          : strings(stringKeys.common.boolean.false)}
-                      </li>
-                      <li>
-                        {strings(
-                          stringKeys.alerts.assess.alert.eidsrValidation
-                            .integrationValid,
-                        )}
-                        :{" "}
-                        {validateEidsrResult?.isIntegrationConfigValid
-                          ? strings(stringKeys.common.boolean.true)
-                          : strings(stringKeys.common.boolean.false)}{" "}
-                      </li>
-                      <li>
-                        {strings(
-                          stringKeys.alerts.assess.alert.eidsrValidation
-                            .validReportsCount,
-                        )}
-                        :{" "}
-                        {validateEidsrResult?.areReportsValidCount
-                          ? validateEidsrResult?.areReportsValidCount
-                          : 0}
-                      </li>
-                    </ul>
-                    <p>
-                      {strings(
-                        stringKeys.alerts.assess.alert.eidsrValidation.tip,
-                      )}
-                    </p>
-                  </Alert>
-                </Snackbar>
-              </>
-            )}
-          </Grid>
-          <Grid item container xs={6} justifyContent="flex-end">
-            <CancelButton onClick={() => props.goToList(projectId)}>
-              {strings(stringKeys.alerts.assess.goBack)}
-            </CancelButton>
-            {!props.isPendingAlertState && (
-              <Fragment>
-                {alertAssessmentStatus === assessmentStatus.toEscalate &&
-                  hasAccess && (
-                    <Fragment>
-                      <AlertsEscalationDialog
-                        alertId={alertId}
-                        escalateAlert={props.escalateAlert}
-                        isEscalating={props.isEscalating}
-                        isFetchingRecipients={props.isFetchingRecipients}
-                        notificationEmails={props.notificationEmails}
-                        notificationPhoneNumbers={
-                          props.notificationPhoneNumbers
-                        }
-                        isOpened={escalationDialogOpened}
-                        close={() => setEscalationDialogOpened(false)}
-                      />
-
-                      <AlertsEscalationWithoutNotificationDialog
-                        alertId={alertId}
-                        escalateAlert={props.escalateAlert}
-                        isEscalating={props.isEscalating}
-                        isOpened={escalationWithoutNotificationDialogOpened}
-                        close={() =>
-                          setEscalationWithoutNotificationDialogOpened(false)
-                        }
-                      />
-
-                      <div
-                        className={styles.escalateWithoutNotificationWrapper}
-                      >
-                        <CheckboxField
-                          name="escalateWithoutNotification"
-                          label={strings(
-                            stringKeys.alerts.assess.alert
-                              .escalateWithoutNotification,
-                          )}
-                          field={form.fields.escalateWithoutNotification}
-                        />
-                      </div>
-
-                      <SubmitButton onClick={handleEscalateAlert}>
-                        {strings(stringKeys.alerts.assess.alert.escalate)}
-                      </SubmitButton>
-                    </Fragment>
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        {isNationalSocietyEidsrEnabled && (
+          <>
+            <div hidden="true">
+              <Button
+                disabled={props.isLoadingValidateEidsr}
+                color="primary"
+                onClick={() => props.validateEidsr(alertId)}
+              >
+                {" "}
+                Validate Eidsr migration{" "}
+              </Button>
+            </div>
+            {props.isLoadingValidateEidsr && <CircularProgress size={12} />}
+            <Snackbar open={openValidationSnackbar}>
+              <Alert
+                style={{ backgroundColor: "#eee" }}
+                onClose={handleCloseValidationSnackbar}
+                severity="info"
+              >
+                <AlertTitle>
+                  {strings(
+                    stringKeys.alerts.assess.alert.eidsrValidation.title,
                   )}
-
-                {(alertAssessmentStatus === assessmentStatus.toDismiss ||
-                  alertAssessmentStatus === assessmentStatus.rejected) &&
-                  hasAccess && (
-                    <SubmitButton
-                      isFetching={props.isDismissing}
-                      onClick={() => handleDismissAlert(alertId)}
-                    >
-                      {strings(stringKeys.alerts.assess.alert.dismiss)}
-                    </SubmitButton>
+                </AlertTitle>
+                <ul>
+                  <li>
+                    {strings(
+                      stringKeys.alerts.assess.alert.eidsrValidation
+                        .connection,
+                    )}
+                    :{" "}
+                    {validateEidsrResult?.isEidsrApiConnectionRunning
+                      ? strings(stringKeys.common.boolean.true)
+                      : strings(stringKeys.common.boolean.false)}
+                  </li>
+                  <li>
+                    {strings(
+                      stringKeys.alerts.assess.alert.eidsrValidation
+                        .integrationValid,
+                    )}
+                    :{" "}
+                    {validateEidsrResult?.isIntegrationConfigValid
+                      ? strings(stringKeys.common.boolean.true)
+                      : strings(stringKeys.common.boolean.false)}{" "}
+                  </li>
+                  <li>
+                    {strings(
+                      stringKeys.alerts.assess.alert.eidsrValidation
+                        .validReportsCount,
+                    )}
+                    :{" "}
+                    {validateEidsrResult?.areReportsValidCount
+                      ? validateEidsrResult?.areReportsValidCount
+                      : 0}
+                  </li>
+                </ul>
+                <p>
+                  {strings(
+                    stringKeys.alerts.assess.alert.eidsrValidation.tip,
                   )}
-
-                {alertAssessmentStatus === assessmentStatus.escalated &&
-                  hasAccess && (
-                    <Fragment>
-                      <AlertsCloseDialog
-                        alertId={alertId}
-                        closeAlert={props.closeAlert}
-                        isClosing={props.isClosing}
-                        isOpened={closeDialogOpened}
-                        close={() => setCloseDialogOpened(false)}
-                      />
-                      <SubmitButton onClick={() => setCloseDialogOpened(true)}>
-                        {strings(stringKeys.alerts.assess.alert.close)}
-                      </SubmitButton>
-                    </Fragment>
-                  )}
-              </Fragment>
-            )}
-          </Grid>
+                </p>
+              </Alert>
+            </Snackbar>
+          </>
+        )}
+      </Grid>
+      <Grid item container justifyContent={isSmallScreen ? "" : "space-between"} direction={isSmallScreen ? "column-reverse" : ""}>
+        <Grid item>
+          <CancelButton onClick={() => props.goToList(projectId)}>
+            {strings(stringKeys.alerts.assess.goBack)}
+          </CancelButton>
         </Grid>
-      </FormActions>
-    </Fragment>
+        {!props.isPendingAlertState && (
+          <Fragment>
+            {alertAssessmentStatus === assessmentStatus.toEscalate &&
+              hasAccess && (
+                <Fragment>
+                  <AlertsEscalationDialog
+                    alertId={alertId}
+                    escalateAlert={props.escalateAlert}
+                    isEscalating={props.isEscalating}
+                    isFetchingRecipients={props.isFetchingRecipients}
+                    notificationEmails={props.notificationEmails}
+                    notificationPhoneNumbers={
+                      props.notificationPhoneNumbers
+                    }
+                    isOpened={escalationDialogOpened}
+                    close={() => setEscalationDialogOpened(false)}
+                  />
+
+                  <AlertsEscalationWithoutNotificationDialog
+                    alertId={alertId}
+                    escalateAlert={props.escalateAlert}
+                    isEscalating={props.isEscalating}
+                    isOpened={escalationWithoutNotificationDialogOpened}
+                    close={() =>
+                      setEscalationWithoutNotificationDialogOpened(false)
+                    }
+                  />
+                <Grid container alignItems="center" item style={{ maxWidth: "fit-content" }}>
+                <Grid item style={{ marginRight: 10 }}>
+                  <CheckboxField
+                    name="escalateWithoutNotification"
+                    label={strings(
+                      stringKeys.alerts.assess.alert
+                      .escalateWithoutNotification,
+                      )}
+                      field={form.fields.escalateWithoutNotification}
+                  />
+                </Grid>
+                <Grid item>
+                  <SubmitButton onClick={handleEscalateAlert}>
+                    {strings(stringKeys.alerts.assess.alert.escalate)}
+                  </SubmitButton>
+                </Grid>
+                </Grid>
+                </Fragment>
+              )}
+
+            {(alertAssessmentStatus === assessmentStatus.toDismiss ||
+              alertAssessmentStatus === assessmentStatus.rejected) &&
+              hasAccess && (
+                <SubmitButton
+                  isFetching={props.isDismissing}
+                  onClick={() => handleDismissAlert(alertId)}
+                >
+                  {strings(stringKeys.alerts.assess.alert.dismiss)}
+                </SubmitButton>
+              )}
+
+            {alertAssessmentStatus === assessmentStatus.escalated &&
+              hasAccess && (
+                <Fragment>
+                  <AlertsCloseDialog
+                    alertId={alertId}
+                    closeAlert={props.closeAlert}
+                    isClosing={props.isClosing}
+                    isOpened={closeDialogOpened}
+                    close={() => setCloseDialogOpened(false)}
+                  />
+                  <SubmitButton onClick={() => setCloseDialogOpened(true)}>
+                    {strings(stringKeys.alerts.assess.alert.close)}
+                  </SubmitButton>
+                </Fragment>
+              )}
+          </Fragment>
+        )}
+      </Grid>
+    </Grid>
   );
 };
