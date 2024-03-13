814eef43bd0153e3e7b2ced2d88d2ad7f61873dc
Ian Evangelista <ian-angelo.roman.evangelista@redcross.no>
2024-03-13 12:34:17 +0100

    Style back button for rtl

--- src/RX.Nyss.Web/ClientApp/src/components/common/buttons/goBackToButton/GoBackToButton.jsx
+++ src/RX.Nyss.Web/ClientApp/src/components/common/buttons/goBackToButton/GoBackToButton.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import { useSelector } from "react-redux";
 
-export const GoBackToButton = ({ onClick, variant, children, size }) => (
-  <Button
-    onClick={onClick}
-    startIcon={<ArrowBackIcon />}
-    variant={variant}
-    color="primary"
-    size={size}
-  >
-    {children}
-  </Button>
-);
+export const GoBackToButton = ({ onClick, variant, children, size }) => {
+  const useRtlDirection = useSelector(
+    (state) => state.appData.direction === "rtl",
+  );
+  const useStyles = makeStyles(() => ({
+    icon: {
+      marginLeft: useRtlDirection ? 8 : "initial",
+      marginRight: useRtlDirection ? -4 : "initial",
+    },
+  }));
+  const classes = useStyles();
+
+  return (
+    <Button
+      onClick={onClick}
+      startIcon={useRtlDirection ? <ArrowForwardIcon /> : <ArrowBackIcon />}
+      classes={{
+        startIcon: useRtlDirection && classes.icon,
+      }}
+      variant={variant}
+      color="primary"
+      size={size}
+    >
+      {children}
+    </Button>
+  );
+};
 
 export default GoBackToButton;
