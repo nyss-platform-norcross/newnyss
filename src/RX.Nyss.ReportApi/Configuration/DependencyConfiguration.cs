d658099f6c0be9132e6077d63bf2f8fa66ad5d08
Taghi Zadehnoveiry <taghi.zadehnoveiry@redcross.no>
2024-07-23 13:27:51 +0200

    Migration of func apps to dotnet 6.0

--- src/RX.Nyss.ReportApi/Configuration/DependencyConfiguration.cs
+++ src/RX.Nyss.ReportApi/Configuration/DependencyConfiguration.cs
@@ -16,115 +16,114 @@ using RX.Nyss.Common.Utils.Logging;
 using RX.Nyss.Data;
 using Serilog;
 
-namespace RX.Nyss.ReportApi.Configuration
+namespace RX.Nyss.ReportApi.Configuration;
+
+public static class DependencyConfiguration
 {
-    public static class DependencyConfiguration
+    public static void ConfigureDependencies(this IServiceCollection serviceCollection, IConfiguration configuration)
     {
-        public static void ConfigureDependencies(this IServiceCollection serviceCollection, IConfiguration configuration)
-        {
-            serviceCollection.AddDatabaseDeveloperPageExceptionFilter();
+        serviceCollection.AddDatabaseDeveloperPageExceptionFilter();
 
-            var config = configuration.Get<ConfigSingleton>();
-            RegisterLogger(serviceCollection, config.Logging, configuration);
-            RegisterDatabases(serviceCollection, config.ConnectionStrings);
-            RegisterWebFramework(serviceCollection);
-            RegisterSwagger(serviceCollection);
-            RegisterServiceCollection(serviceCollection, config);
-            RegisterAzureClients(serviceCollection, config.ConnectionStrings);
-        }
-
-        private static void RegisterAzureClients(IServiceCollection serviceCollection, ConnectionStringOptions connectionStrings) =>
-            serviceCollection.AddAzureClients(builder =>
-            {
-                builder.AddServiceBusClient(connectionStrings.ServiceBus);
-                builder.AddBlobServiceClient(connectionStrings.GeneralBlobContainer)
-                    .WithName("GeneralBlobClient");
-                builder.AddBlobServiceClient(connectionStrings.DataBlobContainer)
-                    .WithName("DataBlobClient");
-            });
-
-        private static void RegisterLogger(IServiceCollection serviceCollection,
-            ILoggingOptions loggingOptions, IConfiguration configuration)
-        {
-            const string applicationInsightsEnvironmentVariable = "APPINSIGHTS_INSTRUMENTATIONKEY";
-            var appInsightsInstrumentationKey = configuration[applicationInsightsEnvironmentVariable];
-            GlobalLoggerConfiguration.ConfigureLogger(loggingOptions, appInsightsInstrumentationKey);
-            serviceCollection.AddSingleton(x => Log.Logger); // must be func, as the static logger is configured (changed reference) after DI registering
-            serviceCollection.AddSingleton<ILoggerAdapter, SerilogLoggerAdapter>();
-
-            if (!string.IsNullOrEmpty(appInsightsInstrumentationKey))
-            {
-                serviceCollection.AddApplicationInsightsTelemetry();
-            }
-        }
-
-        private static void RegisterDatabases(IServiceCollection serviceCollection, IConnectionStringOptions connectionStringOptions) =>
-            serviceCollection.AddDbContext<NyssContext>(options =>
-                options.UseSqlServer(connectionStringOptions.NyssDatabase,
-                    x => x.UseNetTopologySuite()));
-
-        private static void RegisterWebFramework(IServiceCollection serviceCollection)
-        {
-            serviceCollection
-                .AddControllers()
-                .AddJsonOptions(options =>
-                {
-                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-                    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-                })
-                //.AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()))
-                .ConfigureApiBehaviorOptions(options =>
-                {
-                    options.InvalidModelStateResponseFactory = actionContext =>
-                    {
-                        var validationErrors = actionContext.ModelState.Where(v => v.Value.Errors.Count > 0)
-                            .ToDictionary(stateEntry => stateEntry.Key,
-                                stateEntry => stateEntry.Value.Errors.Select(x => x.ErrorMessage));
-
-                        return new BadRequestObjectResult(validationErrors);
-                    };
-                });
-
-            serviceCollection.AddHttpClient();
-        }
-
-        private static void RegisterSwagger(IServiceCollection serviceCollection) =>
-            serviceCollection.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Nyss Report API",
-                    Version = "v1"
-                });
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            });
-
-        private static void RegisterServiceCollection(IServiceCollection serviceCollection, ConfigSingleton nyssReportApiConfig)
-        {
-            serviceCollection.AddSingleton<IConfig>(nyssReportApiConfig);
-            serviceCollection.AddSingleton<INyssReportApiConfig>(nyssReportApiConfig);
-            RegisterTypes(serviceCollection, "RX.Nyss");
-        }
-
-        private static void RegisterTypes(IServiceCollection serviceCollection, string namePrefix) =>
-            GetAssemblies(namePrefix: namePrefix)
-                .SelectMany(assembly => assembly.GetExportedTypes())
-                .Select(type => new
-                {
-                    implementationType = type,
-                    interfaceType = type.GetInterfaces().FirstOrDefault(i => i.Name == $"I{type.Name}")
-                })
-                .Where(x => x.interfaceType != null)
-                .ToList()
-                .ForEach(i => serviceCollection.AddScoped(i.interfaceType, i.implementationType));
-
-        private static Assembly[] GetAssemblies(string namePrefix) =>
-            DependencyContext.Default.GetDefaultAssemblyNames()
-                .Where(name => name.Name.StartsWith(namePrefix))
-                .Select(Assembly.Load)
-                .ToArray();
+        var config = configuration.Get<ConfigSingleton>();
+        RegisterLogger(serviceCollection, config.Logging, configuration);
+        RegisterDatabases(serviceCollection, config.ConnectionStrings);
+        RegisterWebFramework(serviceCollection);
+        RegisterSwagger(serviceCollection);
+        RegisterServiceCollection(serviceCollection, config);
+        RegisterAzureClients(serviceCollection, config.ConnectionStrings);
     }
+
+    private static void RegisterAzureClients(IServiceCollection serviceCollection, ConnectionStringOptions connectionStrings) =>
+        serviceCollection.AddAzureClients(builder =>
+        {
+            builder.AddServiceBusClient(connectionStrings.ServiceBus);
+            builder.AddBlobServiceClient(connectionStrings.GeneralBlobContainer)
+                .WithName("GeneralBlobClient");
+            builder.AddBlobServiceClient(connectionStrings.DataBlobContainer)
+                .WithName("DataBlobClient");
+        });
+
+    private static void RegisterLogger(IServiceCollection serviceCollection,
+        ILoggingOptions loggingOptions, IConfiguration configuration)
+    {
+        const string applicationInsightsEnvironmentVariable = "APPINSIGHTS_INSTRUMENTATIONKEY";
+        var appInsightsInstrumentationKey = configuration[applicationInsightsEnvironmentVariable];
+        GlobalLoggerConfiguration.ConfigureLogger(loggingOptions, appInsightsInstrumentationKey);
+        serviceCollection.AddSingleton(x => Log.Logger); // must be func, as the static logger is configured (changed reference) after DI registering
+        serviceCollection.AddSingleton<ILoggerAdapter, SerilogLoggerAdapter>();
+
+        if (!string.IsNullOrEmpty(appInsightsInstrumentationKey))
+        {
+            serviceCollection.AddApplicationInsightsTelemetry();
+        }
+    }
+
+    private static void RegisterDatabases(IServiceCollection serviceCollection, IConnectionStringOptions connectionStringOptions) =>
+        serviceCollection.AddDbContext<NyssContext>(options =>
+            options.UseSqlServer(connectionStringOptions.NyssDatabase,
+                x => x.UseNetTopologySuite()));
+
+    private static void RegisterWebFramework(IServiceCollection serviceCollection)
+    {
+        serviceCollection
+            .AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            })
+            //.AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()))
+            .ConfigureApiBehaviorOptions(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var validationErrors = actionContext.ModelState.Where(v => v.Value.Errors.Count > 0)
+                        .ToDictionary(stateEntry => stateEntry.Key,
+                            stateEntry => stateEntry.Value.Errors.Select(x => x.ErrorMessage));
+
+                    return new BadRequestObjectResult(validationErrors);
+                };
+            });
+
+        serviceCollection.AddHttpClient();
+    }
+
+    private static void RegisterSwagger(IServiceCollection serviceCollection) =>
+        serviceCollection.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Nyss Report API",
+                Version = "v1"
+            });
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
+
+    private static void RegisterServiceCollection(IServiceCollection serviceCollection, ConfigSingleton nyssReportApiConfig)
+    {
+        serviceCollection.AddSingleton<IConfig>(nyssReportApiConfig);
+        serviceCollection.AddSingleton<INyssReportApiConfig>(nyssReportApiConfig);
+        RegisterTypes(serviceCollection, "RX.Nyss");
+    }
+
+    private static void RegisterTypes(IServiceCollection serviceCollection, string namePrefix) =>
+        GetAssemblies(namePrefix: namePrefix)
+            .SelectMany(assembly => assembly.GetExportedTypes())
+            .Select(type => new
+            {
+                implementationType = type,
+                interfaceType = type.GetInterfaces().FirstOrDefault(i => i.Name == $"I{type.Name}")
+            })
+            .Where(x => x.interfaceType != null)
+            .ToList()
+            .ForEach(i => serviceCollection.AddScoped(i.interfaceType, i.implementationType));
+
+    private static Assembly[] GetAssemblies(string namePrefix) =>
+        DependencyContext.Default.GetDefaultAssemblyNames()
+            .Where(name => name.Name.StartsWith(namePrefix))
+            .Select(Assembly.Load)
+            .ToArray();
 }
--- src/RX.Nyss.Web/Data/ApplicationDbContext.cs
+++ src/RX.Nyss.Web/Data/ApplicationDbContext.cs
@@ -3,22 +3,21 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace RX.Nyss.Web.Data
+namespace RX.Nyss.Web.Data;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>, IDataProtectionKeyContext
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>, IDataProtectionKeyContext
+    public ApplicationDbContext(
+        DbContextOptions options) : base(options)
     {
-        public ApplicationDbContext(
-            DbContextOptions options) : base(options)
-        {
-        }
+    }
 
-        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+    public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.HasDefaultSchema("identity");
-            modelBuilder.Seed();
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasDefaultSchema("identity");
+        modelBuilder.Seed();
     }
 }
